9f42386cdef5a1023abd5c59a1e91afb546cd642
Roland Clarion <131647107+yoyan1@users.noreply.github.com>
2024-02-24 14:06:42 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BooksView from '../views/BooksView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/viewBooks',
       name: 'books details',
-      component: () => import('../views/BooksView.vue')
+      component: BooksView
     }
   ]
 })
